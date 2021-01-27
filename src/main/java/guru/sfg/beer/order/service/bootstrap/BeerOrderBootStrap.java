87eb92ffb890ce2d49e8686ad655d46f3bd6be04
Pedro <pmancerons@gmail.com>
2021-01-27 17:28:04 -0500

    adding constructor to fix a bug in deserialization

--- src/main/java/guru/sfg/beer/order/service/bootstrap/BeerOrderBootStrap.java
+++ src/main/java/guru/sfg/beer/order/service/bootstrap/BeerOrderBootStrap.java
@@ -29,7 +29,7 @@ public class BeerOrderBootStrap implements CommandLineRunner {
     }
 
     private void loadCustomerData() {
-        if (customerRepository.count() == 0) {
+        if (customerRepository.findAllByCustomerNameLike(TASTING_ROOM).size() == 0) {
             Customer saved = customerRepository.save(Customer.builder()
                     .customerName(TASTING_ROOM)
                     .apiKey(UUID.randomUUID())
